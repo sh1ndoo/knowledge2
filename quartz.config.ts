8bcd82889db821cc71a64091ae4daeef506a4d7f
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-05-20 01:43:53 +0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -136,7 +136,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.Citations({ bibliographyFile: "./content/bibliography.bib", linkCitations: true }),
+      // Plugin.Citations({ bibliographyFile: "./content/bibliography.bib", linkCitations: true }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
