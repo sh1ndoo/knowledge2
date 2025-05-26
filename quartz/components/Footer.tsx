b2d03e88027fb9a726b2320d8a2dd797a192a385
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-05-26 23:58:16 +0700

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -3,7 +3,8 @@ import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
 // @ts-ignore
-import script from "./scripts/_randomPage.inline"
+import randomPageScript from "./scripts/_randomPage.inline"
+import viewsCountScript from "./scripts/_viewsCount.inline"
 
 interface Options {
   links: Record<string, string>
@@ -20,7 +21,6 @@ export default ((opts?: Options) => {
         {/*  {i18n(cfg.locale).components.footer.createdWith}{" "}*/}
         {/*  <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}*/}
         {/*</p>*/}
-        <p></p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
@@ -41,11 +41,14 @@ export default ((opts?: Options) => {
             </a>
           </li>
         </ul>
+        <ul>
+          <p>👀 <a id="site-total-visits"></a>  👀</p>
+        </ul>
       </footer>
     )
   }
 
   Footer.css = style
-  Footer.afterDOMLoaded = script
+  Footer.afterDOMLoaded = [randomPageScript, viewsCountScript]
   return Footer
 }) satisfies QuartzComponentConstructor
