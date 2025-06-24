1172dc241bd3a62ea86dddb4dfa5ba4c9265e11f
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-06-25 01:11:46 +0700

    Update search.inline.ts

--- quartz/components/scripts/search.inline.ts
+++ quartz/components/scripts/search.inline.ts
@@ -18,7 +18,6 @@ let currentSearchTerm: string = ""
 const encoder = (str: string) => str.toLowerCase().split(/([^a-z]|[^\x00-\x7F])/)
 let index = new FlexSearch.Document<Item>({
   charset: "latin:extra",
-  encode: encoder,
   document: {
     id: "id",
     tag: "tags",
